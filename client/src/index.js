8c7c6572d2372f180441026e923113204ceef69b
Allenn George <allenngeorge@Allenns-MacBook-Pro.local>
2025-08-05 02:27:17 +0530

    Restore real F&O prediction app

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import DebugApp from "./DebugApp.jsx"; // Debug version to identify Vercel issues
+import App from "./App.jsx"; // Back to your real app
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <DebugApp />
+    <App />
   </React.StrictMode>
 );
